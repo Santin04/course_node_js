00ac2f0178d3825b6671e753b6ffbbb136cf8693
Santin04 <masantinte@gmail.com>
2024-05-26 16:37:47 -0300

    aula-11

--- 01_basic/aula11.js
+++ 01_basic/aula11.js
@@ -1,9 +1,13 @@
+//importando e configurando a biblioteca que permite fazer perguntas para o usuário dentro do terminal
 const readline = require('readline').createInterface({
     input: process.stdin,
     output: process.stdout,
 })
 
+//fazendo a pergunta de qual a linguagem preferida do usuário
 readline.question('Qual a sua linguagem preferida? ', (linguagem) => {
+    //exibindo para o usuário qual a linguagem preferida dele
     console.log('A sua linguagem preferida é ' + linguagem);
+    //fechando o questeonário
     readline.close();
 })
