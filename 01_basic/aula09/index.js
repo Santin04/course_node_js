af443bcfe6ffe7e0f9c0549f3ce75d2d114bf60e
Santin04 <masantinte@gmail.com>
2024-05-26 15:15:47 -0300

    aula-09

---
+++ 01_basic/aula09/index.js
@@ -0,0 +1,23 @@
+//criando variaveis de diversos tipos
+const x = 10;
+const y = 'conversa';
+const z = [1, 2];
+
+//exibindo no console os valores das variaveis
+console.log(x, y, z);
+
+//contagem de quantas vezes aquele dado apareceu no console
+console.count(y);
+console.count(y);
+console.count(y);
+//quando troca o valor que exibe no console, ele gera uma nova contagem
+console.count(z);
+//aqui como já teve 3 antes ele continua no valor 4
+console.count(y);
+
+//passando o valor depois da string
+console.log('oi, %s, tchau', y);
+//desse jeito o %s, indica que ali vai ter uma variavel tipo string, e é só passar a variavel depois da virgula
+
+//limpa o console
+console.clear()
