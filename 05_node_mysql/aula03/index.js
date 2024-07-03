9f403f9912ac824cfb88629f93562391309f5ae6
Santin04 <masantinte@gmail.com>
2024-07-03 17:39:09 -0300

    aula-03

---
+++ 05_node_mysql/aula03/index.js
@@ -0,0 +1,84 @@
+//nesse arquivo vamos puxar dados do banco de dados
+
+const express = require('express');
+const app = express();
+const exphbs = require('express-handlebars');
+const mysql = require('mysql');
+const port = 3000;
+
+//configurando para usar handlebars
+app.engine('handlebars', exphbs.engine());
+app.set('view engine', 'handlebars');
+
+//deixando arquivos staticos acessíveis
+app.use(express.static('public'));
+
+//preparando para receber um post caso tenha
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(express.json());
+
+//criando a conexão com o banco de dados
+const conn = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database: 'nodemysql2',
+    password: ''
+});
+
+//depois de configurar tudo começo a mexer com os caminhos get e post
+
+app.get('/', (req, res) => {
+    res.render('home');
+})
+
+app.post('/books/insertbook', (req, res) => {
+    const title = req.params.title;
+    const pageqty = req.param.pageqty;
+
+    const sql = `INSERT INTO books (title, pageqty) VALUES ('${title}', '${pageqty}')`;
+
+    conn.query(sql, (err) => {
+        if(err){
+            console.log('Erro ao enviar para o banco de dados');
+            return;
+        }
+
+        res.redirect('/');
+    });
+})
+
+//vamos fazerr um get aqui quando o usuário for para tal url, e quando ele for vamos dar como res todos os books
+app.get('/books', (req, res) => {
+    //fazer a query que vai selecionar todos as linhas da tabela books
+    //straduzindo: selecionar todos elemento de books
+    const sql = 'SELECT * FROM books';
+
+    //enviando a query para o banco de dados para ela ser executada
+    conn.query(sql, (err, data) => {
+        //verificando se teve algum erro na execução da query
+        if(err) {
+            console.log('Erro ao puxar os dados');
+            return;
+        }
+
+        //definindo o conteudo que foi pego do banco de dados para dentro de uma variavel
+        const books = data;
+
+        //exibindo o arquivo handlebars chamado books e passando os dados obtidos do banco de dados para poder exibi-los
+        res.render('books', {books : books});
+    });
+});
+
+conn.connect((err) => {
+    if(err) {
+        console.log('Erro ao conetar com o banco de dados');
+        return;
+    }
+    console.log('Conectado ao banco de dados');
+
+    app.listen(port, () => {
+        console.log('Servido rodando');
+    })
+})
